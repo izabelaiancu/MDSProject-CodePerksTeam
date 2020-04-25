0a5887b9578c52d67cde4bd232255ea1715417be
Izabela Iancu <izabela.iancu14@gmail.com>
2020-04-25 20:17:24 +0300

    -> am inclus dependintele necesare in pom.xml  pentru conexiunea cu baza de date mysql -> in application.properties am inclus credentialele pentru conexiune

--- src/main/java/com/mds/mds/MdsApplication.java
+++ src/main/java/com/mds/mds/MdsApplication.java
@@ -2,12 +2,15 @@ package com.mds.mds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class MdsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MdsApplication.class, args);
+        System.out.println("merge");
     }
+
 
 }
